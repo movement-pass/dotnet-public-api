38b8e9e542333afe29a6fb10fbf2b47079c7f40e
Kazi Manzur Rashid <kazimanzurrashid@gmail.com>
2021-09-11 22:33:29 +0600

    chore: removed background job to move to separate repo

--- MovementPass.Public.Api.BackgroundJob/Infrastructure/Clock.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MovementPass.Public.Api.BackgroundJob.Infrastructure
-{
-    using System;
-
-    public static class Clock
-    {
-        public static Func<DateTime> Now { get; set; } = () => DateTime.UtcNow;
-    }
-}
